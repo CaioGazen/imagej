41a11271dcb0c3e08ff13e7c64263031b531358a
Caio Gazen <61031028+CaioGazen@users.noreply.github.com>
2025-01-28 19:16:03 -0300

    initial commit

---
+++ customPlugins/TestPlugin_.java
@@ -0,0 +1,8 @@
+import ij.*;
+import ij.plugin.PlugIn;
+
+public class TestPlugin_ implements PlugIn {
+  public void run(String arg) {
+    IJ.error("Não falei que tinha um jeito mais fácil");
+  }
+}
