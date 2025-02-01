624f68463017cae6aaa0d6448871421dfbe67d01
Caio Gazen <61031028+CaioGazen@users.noreply.github.com>
2025-02-01 16:59:09 -0300

    idk

--- customPlugins/TestPlugin_.java
+++
@@ -1,8 +0,0 @@
-import ij.*;
-import ij.plugin.PlugIn;
-
-public class TestPlugin_ implements PlugIn {
-  public void run(String arg) {
-    IJ.error("Não falei que tinha um jeito mais fácil");
-  }
-}
